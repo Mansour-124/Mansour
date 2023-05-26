016ad81e214a7e57f5de3b267a5843f92270416c
Mansour-124 <61943407+Mansour-124@users.noreply.github.com>
2023-05-26 22:03:02 +0300

    Add files via upload

---
+++ home.php
@@ -0,0 +1,217 @@
+<!DOCTYPE html>
+
+<html> 
+    <head>
+        
+        <title> Travel to enjoy</title>
+        <meta charest="UTF-8">
+        <link rel="stylesheet" href="css/style.css" type="text/css"/>
+ 		 <link rel="stylesheet" href="css/font-awesome.min.css" type="text/css"/>
+    </head>
+<body>
+	
+
+    <div class="div1">
+ 
+       
+            <p id="id1" title="neme of this site"><i><span class="yellow">T</span>ravel</i></p>
+
+            <ul style="margin-top :0px;">
+                <li><a href="team.html"><i class="fa fa-group"></i> Team</a></li>
+                <li><a href="Menu.html"><i class="fa fa-bars"></i> Menu</a></li>
+                <li> <a href="Save%20Bus.html"><i class="fa fa-save"></i> Save Bus</a> </li>
+                <li> <a href="Contact%20Us.html"><i class="fa fa-address-book"></i> Contact Us</a> </li>
+                <li> <a href="sign%20in.html"><i class="fa fa-sign-in"></i> Sign In</a> </li>   
+                <li><a href="home.html"><i class="fa fa-home"></i> Home</a></li>
+            </ul>
+    
+        
+    </div>
+    <div>
+               <h1 id="id2">
+                   <i><i><span class="yellow">T</span>ravel</i></i>
+               </h1>
+               <p id="id3">
+                   <i> VIP || Weeding || Party || Original ||Transportation || Business </i>
+               </p>
+                <video id="video"width="80%" height="750px" controls>
+                <source src="videos/bus.mp4" type="video/mp4">
+                <source src="videos/bus.mp4" type="video/ogg">
+                     Your browser does not support the video tag.
+                </video>
+        <br><br>
+               <button class="button" id="story"><a href="#id6">Discover story </a></button>
+   
+    </div>
+    <div >
+        <h1 id="id4">
+            <i> we are the number1</i>
+        </h1>
+    </div>
+    <br>
+    <div>
+        <dl>
+            <dd>
+           <img src="images/hd.JPG" id="img--"> 
+            </dd>
+              <dd>
+           <img src="images/cv.jpg" id="img--"> 
+            </dd>
+              <dd>
+           <img src="images/uu.jpg" id="img--"> 
+            </dd>
+              <dd>
+           <img src="images/hw.jpg" id="img--"> 
+            </dd>
+             <dd>
+           <img src="images/beauty.jpg" id="img--"> 
+            </dd>
+            
+        </dl>
+    </div>
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <div class="botttom">
+        <div>
+            <h3 id="id6"><span style="color:olive;">O</span>ur <span style="color:olive;">S</span>ervices</h3>
+        </div>
+ 
+            <ol>   
+                <li>
+                <img src="images/vip.jpg" id="images_bus">
+                <h3 id="img_1"> VIP Buses</h3>
+                <p style="color:chocolate"> This paragraph
+                    contains a lot of lines
+                    in the source code,
+                    <br>but the browser 
+                    ignores it. This paragraph
+                    contains a lot<br>
+                    of lines
+                    in the source code,
+                    but the browser 
+                    ignores it.<br>
+                    This paragraph
+                    contains a lot of lines
+                    in the source code,
+                    <br>but the browser 
+                    ignores it.
+                </p>
+                </li>
+                
+                <li>
+                <img src="images/weeding.jpg" id="images_bus">
+                <h3 id="img_2">  Weeding Buses</h3>
+                <p style="color:black;"> This paragraph
+                    contains a lot of lines
+                    in the source code,
+                    <br>but the browser 
+                    ignores it. This paragraph
+                    contains a lot<br> of lines
+                    in the source code,
+                    but the browser 
+                    ignores it.<br>
+                    This paragraph
+                    contains a lot of lines
+                    in the source code,
+                    <br>but the browser 
+                    ignores it.
+                </p>
+                </li>
+                <li>
+                <img src="images/party.jpg" id="images_bus">
+                <h3 id="img_3"> Party Buses</h3>
+                <p style="color:darkslateblue;"> This paragraph
+                    contains a lot of lines
+                    in the source code,
+                    <br>but the browser 
+                    ignores it. This paragraph
+                    contains a lot <br>of lines
+                    in the source code,
+                    but the browser 
+                    ignores it.<br>
+                    This paragraph
+                    contains a lot of lines
+                    in the source code,
+                    <br>but the browser 
+                    ignores it.
+                </p>
+                </li>
+                <br>
+                 <li>
+                <img src="images/business.jpg" id="images_bus">
+                <h3 id="img_4">  Business Buses</h3>
+                <p style="color:firebrick"> This paragraph
+                    contains a lot of lines
+                    in the source code,
+                    <br>but the browser 
+                    ignores it. This paragraph
+                    contains a lot<br>
+                    of lines
+                    in the source code,
+                    but the browser 
+                    ignores it.<br>
+                    This paragraph
+                    contains a lot of lines
+                    in the source code,
+                    <br>but the browser 
+                    ignores it.
+                </p>
+                </li>
+                <li>
+                <img src="images/death.jpg" id="images_bus">
+                <h3 id="img_5">  Death Buses </h3>
+                <p style="color:darkslategray"> This paragraph
+                    contains a lot of lines
+                    in the source code,
+                    <br>but the browser 
+                    ignores it. This paragraph
+                    contains a lot<br> of lines
+                    in the source code,
+                    but the browser 
+                    ignores it.<br>
+                    This paragraph
+                    contains a lot of lines
+                    in the source code,
+                    <br>but the browser 
+                    ignores it.
+                </p>
+                </li>
+                <li>
+                <img src="images/transport.jpg" id="images_bus" >
+                <h3 id="img_6">  Transport Buse </h3>
+                <p style="color:darkturquoise"> This paragraph
+                    contains a lot of lines
+                    in the source code,
+                    <br>but the browser 
+                    ignores it. This paragraph
+                    contains a lot <br>of lines
+                    in the source code,
+                    but the browser 
+                    ignores it.<br>
+                    This paragraph
+                    contains a lot of lines
+                    in the source code,
+                    <br>but the browser 
+                    ignores it.
+                </p>
+                </li>
+            
+            
+            </ol>
+</div>
+     <div id="last_fot">
+        <footer>
+        	  <div class="icons">
+        	 	<a href="#"> <i class="fa fa-twitter" id="library"></i></a>
+        	 	<a href="#" ><i class="fa fa-instagram" id="instgoog"></i></a>
+                <a href="#" ><i class="fa fa-facebook" id="library"></i></a>
+        	 	<a href="#" ><i class="fa fa-google-plus-official" id="instgoog"></i></a>
+        	  </div>
+                  <p id="foooot">
+                     copyright &copy; 2019 Travél. All rights reserved. Designed by
+                      <a href="#" >  <span id="oka_foot" >OKASHA</span></a>
+                 </p>
+        </footer>
+    </div>
+    
+</body>
+</html>
